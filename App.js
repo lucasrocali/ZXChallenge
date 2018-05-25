3db94d24f5c7818fec7f5b7c5daa7afc81a77268
Lucas Rocali <rocali@outlook.com>
2018-05-25 04:53:21 -0300

    setup apollo

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import RootNavigation from './app/navigation/RootNavigation';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'https://803votn6w7.execute-api.us-west-2.amazonaws.com/dev/public/graphql',
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <RootNavigation/>
+      <ApolloProvider client={client}>
+        <RootNavigation/>
+      </ApolloProvider>
     );
   }
 }
